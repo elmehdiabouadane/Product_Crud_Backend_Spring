ae34221f5348eb21cb461037af7ce45dd5b6781c
ABOUADANE El Mehdi <hidou-abou@live.fr>
2019-11-09 21:19:46 +0100

    Creating methods

--- src/main/java/com/sid/catservice/CatServiceApplication.java
+++ src/main/java/com/sid/catservice/CatServiceApplication.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class CatServiceApplication implements CommandLineRunner {
 
     @Autowired
     private ProduitRepository produitRepository;
+    @Autowired
+    private RepositoryRestConfiguration restConfiguration;
 
     public static void main(String[] args) {
         SpringApplication.run(CatServiceApplication.class, args);
@@ -19,6 +22,7 @@ public class CatServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        restConfiguration.exposeIdsFor(Produit.class);
         produitRepository.save(new Produit(null, "Dell", 7500, 3));
         produitRepository.save(new Produit(null, "Toshiba", 6000, 5));
         produitRepository.save(new Produit(null, "Asus", 5500, 8));
